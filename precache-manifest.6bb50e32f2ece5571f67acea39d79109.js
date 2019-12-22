5e5bd02d0a360484519feffffd07aaeeeb9546ff
TressOhi <fortressenebe@gmail.com>
2019-12-22 13:17:25 +0100

    Updates

---
+++ precache-manifest.6bb50e32f2ece5571f67acea39d79109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66b6bb9b3a4d6e6d3d406c52d5e826f",
+    "url": "/GP-Meter/index.html"
+  },
+  {
+    "revision": "522d9e5e7a9883153ad9",
+    "url": "/GP-Meter/static/css/main.f4aed8ed.chunk.css"
+  },
+  {
+    "revision": "6ea7a2a11b01f16ba11d",
+    "url": "/GP-Meter/static/js/2.515d3bfe.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/GP-Meter/static/js/2.515d3bfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "522d9e5e7a9883153ad9",
+    "url": "/GP-Meter/static/js/main.2eb7c751.chunk.js"
+  },
+  {
+    "revision": "e711815a36fa18a65462",
+    "url": "/GP-Meter/static/js/runtime-main.af8da2d6.js"
+  }
+]);
